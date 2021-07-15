17d4c8ac85839767f1fc7832740c6456cdf378a2
Wojtek <68703185+wojciecholejnik@users.noreply.github.com>
2021-07-15 10:03:26 +0200

    add node-sass preprocessor adn geting data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.scss';
 
 function App() {
-  return (
-    <div className="App">
+const [post, setPost] = useState(null);
+const [users, setUsers] = useState(null);
 
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => setPost(data))
+}, []);
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => setUsers(data))
+}, []);
+
+  return (
+    <div className={styles.app}>
+      <div></div>
     </div>
   );
 }
